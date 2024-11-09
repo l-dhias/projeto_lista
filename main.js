40ca6b8c8ea951c6dfd0ed12693dd51b2e2ca95c
l-dhias <rpleoddiass@gmail.com>
2024-11-09 12:40:13 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('for-cantato')
+    let linhas = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const inputNomeContato = document.getElementById('nome-contato')
+    const inputNumeroContato = document.getElementById('numero-contato')
+
+
+    let linha = '<tr>'
+    linha += `<td>${inputNomeContato.value}</td>`
+    linha += `<td>${inputNumeroContato.value}</td>`
+    linha += '</tr>'
+
+    linhas += linha
+
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+    
+    inputNomeContato.value = ''
+    inputNumeroContato.value = ''
+
+}) 
